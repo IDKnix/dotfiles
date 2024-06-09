12b05848b096174bb50dd6be6161740db90648d2
IDKnix <149285820+IDKnix@users.noreply.github.com>
2024-06-09 13:05:58 +0800

    Update startlaunch.sh

--- home/startlaunch.sh
+++ home/startlaunch.sh
@@ -2,8 +2,8 @@ xsetroot -cursor_name left_ptr &
 
 nitrogen --restore &
 
-plank &
+xrandr -s 1280x720 && xrandr --output HDMI1 --mode 1280x720
 
-picom -b
+picom -b --experimental-backends
 
 dunstify "Boot time" "Boot took $(python3 ~/boottime.py)"
