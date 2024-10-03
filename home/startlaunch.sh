1f799e8a6224de4695416cdd71b7a397be359465
IDKnix <149285820+IDKnix@users.noreply.github.com>
2024-10-03 16:16:55 +0800

    Delete home/startlaunch.sh

--- home/startlaunch.sh
+++
@@ -1,9 +0,0 @@
-xsetroot -cursor_name left_ptr &
-
-nitrogen --restore &
-
-xrandr -s 1280x720 && xrandr --output HDMI1 --mode 1280x720
-
-picom -b --experimental-backends
-
-dunstify "Boot time" "Boot took $(python3 ~/boottime.py)"
