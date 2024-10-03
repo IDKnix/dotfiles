35429e86eed590699cf08b8a51e177778413ad6f
IDKnix <149285820+IDKnix@users.noreply.github.com>
2024-10-03 16:16:46 +0800

    Delete home/boottime.py

--- home/boottime.py
+++
@@ -1,7 +0,0 @@
-# by IDKnix on the 14 / 5 / 24
-
-import subprocess
-
-timestr = subprocess.run(["systemd-analyze"], shell = True, text = True, capture_output=True, executable="/bin/bash").stdout
-
-print(timestr[99:-55])
